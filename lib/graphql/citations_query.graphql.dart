b2c3f5c8eed56abe88e32e49506b707a57496808
Margarita Krutikova <margaretkru@gmail.com>
2020-03-11 22:14:50 +0100

    Add graphql query for citations and generated types

---
+++ lib/graphql/citations_query.graphql.dart
@@ -0,0 +1,196 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:meta/meta.dart';
+import 'package:artemis/artemis.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:equatable/equatable.dart';
+import 'package:gql/ast.dart';
+part 'citations_query.graphql.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class Citations$query_root$collections$citations$authors with EquatableMixin {
+  Citations$query_root$collections$citations$authors();
+
+  factory Citations$query_root$collections$citations$authors.fromJson(
+          Map<String, dynamic> json) =>
+      _$Citations$query_root$collections$citations$authorsFromJson(json);
+
+  int id;
+
+  String name;
+
+  @override
+  List<Object> get props => [id, name];
+  Map<String, dynamic> toJson() =>
+      _$Citations$query_root$collections$citations$authorsToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class Citations$query_root$collections$citations with EquatableMixin {
+  Citations$query_root$collections$citations();
+
+  factory Citations$query_root$collections$citations.fromJson(
+          Map<String, dynamic> json) =>
+      _$Citations$query_root$collections$citationsFromJson(json);
+
+  int id;
+
+  String text;
+
+  Citations$query_root$collections$citations$authors author;
+
+  @override
+  List<Object> get props => [id, text, author];
+  Map<String, dynamic> toJson() =>
+      _$Citations$query_root$collections$citationsToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class Citations$query_root$collections with EquatableMixin {
+  Citations$query_root$collections();
+
+  factory Citations$query_root$collections.fromJson(
+          Map<String, dynamic> json) =>
+      _$Citations$query_root$collectionsFromJson(json);
+
+  List<Citations$query_root$collections$citations> citations;
+
+  @override
+  List<Object> get props => [citations];
+  Map<String, dynamic> toJson() =>
+      _$Citations$query_root$collectionsToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class Citations$query_root with EquatableMixin {
+  Citations$query_root();
+
+  factory Citations$query_root.fromJson(Map<String, dynamic> json) =>
+      _$Citations$query_rootFromJson(json);
+
+  List<Citations$query_root$collections> collections;
+
+  @override
+  List<Object> get props => [collections];
+  Map<String, dynamic> toJson() => _$Citations$query_rootToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class CitationsArguments extends JsonSerializable with EquatableMixin {
+  CitationsArguments({@required this.slug});
+
+  factory CitationsArguments.fromJson(Map<String, dynamic> json) =>
+      _$CitationsArgumentsFromJson(json);
+
+  final String slug;
+
+  @override
+  List<Object> get props => [slug];
+  Map<String, dynamic> toJson() => _$CitationsArgumentsToJson(this);
+}
+
+class CitationsQuery
+    extends GraphQLQuery<Citations$query_root, CitationsArguments> {
+  CitationsQuery({this.variables});
+
+  @override
+  final DocumentNode document = DocumentNode(definitions: [
+    OperationDefinitionNode(
+        type: OperationType.query,
+        name: NameNode(value: 'Citations'),
+        variableDefinitions: [
+          VariableDefinitionNode(
+              variable: VariableNode(name: NameNode(value: 'slug')),
+              type: NamedTypeNode(
+                  name: NameNode(value: 'String'), isNonNull: true),
+              defaultValue: DefaultValueNode(value: null),
+              directives: [])
+        ],
+        directives: [],
+        selectionSet: SelectionSetNode(selections: [
+          FieldNode(
+              name: NameNode(value: 'collections'),
+              alias: null,
+              arguments: [
+                ArgumentNode(
+                    name: NameNode(value: 'where'),
+                    value: ObjectValueNode(fields: [
+                      ObjectFieldNode(
+                          name: NameNode(value: 'slug'),
+                          value: ObjectValueNode(fields: [
+                            ObjectFieldNode(
+                                name: NameNode(value: '_eq'),
+                                value:
+                                    VariableNode(name: NameNode(value: 'slug')))
+                          ]))
+                    ]))
+              ],
+              directives: [],
+              selectionSet: SelectionSetNode(selections: [
+                FieldNode(
+                    name: NameNode(value: 'citations'),
+                    alias: null,
+                    arguments: [
+                      ArgumentNode(
+                          name: NameNode(value: 'order_by'),
+                          value: ObjectValueNode(fields: [
+                            ObjectFieldNode(
+                                name: NameNode(value: 'added'),
+                                value: EnumValueNode(
+                                    name: NameNode(value: 'desc'))),
+                            ObjectFieldNode(
+                                name: NameNode(value: 'id'),
+                                value: EnumValueNode(
+                                    name: NameNode(value: 'desc')))
+                          ]))
+                    ],
+                    directives: [],
+                    selectionSet: SelectionSetNode(selections: [
+                      FieldNode(
+                          name: NameNode(value: 'id'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: null),
+                      FieldNode(
+                          name: NameNode(value: 'text'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: null),
+                      FieldNode(
+                          name: NameNode(value: 'author'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: SelectionSetNode(selections: [
+                            FieldNode(
+                                name: NameNode(value: 'id'),
+                                alias: null,
+                                arguments: [],
+                                directives: [],
+                                selectionSet: null),
+                            FieldNode(
+                                name: NameNode(value: 'name'),
+                                alias: null,
+                                arguments: [],
+                                directives: [],
+                                selectionSet: null)
+                          ]))
+                    ]))
+              ]))
+        ]))
+  ]);
+
+  @override
+  final String operationName = 'Citations';
+
+  @override
+  final CitationsArguments variables;
+
+  @override
+  List<Object> get props => [document, operationName, variables];
+  @override
+  Citations$query_root parse(Map<String, dynamic> json) =>
+      Citations$query_root.fromJson(json);
+}
---
+++ lib/graphql/citations_query.graphql.g.dart
@@ -0,0 +1,86 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'citations_query.graphql.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Citations$query_root$collections$citations$authors
+    _$Citations$query_root$collections$citations$authorsFromJson(
+        Map<String, dynamic> json) {
+  return Citations$query_root$collections$citations$authors()
+    ..id = json['id'] as int
+    ..name = json['name'] as String;
+}
+
+Map<String, dynamic> _$Citations$query_root$collections$citations$authorsToJson(
+        Citations$query_root$collections$citations$authors instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+    };
+
+Citations$query_root$collections$citations
+    _$Citations$query_root$collections$citationsFromJson(
+        Map<String, dynamic> json) {
+  return Citations$query_root$collections$citations()
+    ..id = json['id'] as int
+    ..text = json['text'] as String
+    ..author = json['author'] == null
+        ? null
+        : Citations$query_root$collections$citations$authors.fromJson(
+            json['author'] as Map<String, dynamic>);
+}
+
+Map<String, dynamic> _$Citations$query_root$collections$citationsToJson(
+        Citations$query_root$collections$citations instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'text': instance.text,
+      'author': instance.author?.toJson(),
+    };
+
+Citations$query_root$collections _$Citations$query_root$collectionsFromJson(
+    Map<String, dynamic> json) {
+  return Citations$query_root$collections()
+    ..citations = (json['citations'] as List)
+        ?.map((e) => e == null
+            ? null
+            : Citations$query_root$collections$citations.fromJson(
+                e as Map<String, dynamic>))
+        ?.toList();
+}
+
+Map<String, dynamic> _$Citations$query_root$collectionsToJson(
+        Citations$query_root$collections instance) =>
+    <String, dynamic>{
+      'citations': instance.citations?.map((e) => e?.toJson())?.toList(),
+    };
+
+Citations$query_root _$Citations$query_rootFromJson(Map<String, dynamic> json) {
+  return Citations$query_root()
+    ..collections = (json['collections'] as List)
+        ?.map((e) => e == null
+            ? null
+            : Citations$query_root$collections.fromJson(
+                e as Map<String, dynamic>))
+        ?.toList();
+}
+
+Map<String, dynamic> _$Citations$query_rootToJson(
+        Citations$query_root instance) =>
+    <String, dynamic>{
+      'collections': instance.collections?.map((e) => e?.toJson())?.toList(),
+    };
+
+CitationsArguments _$CitationsArgumentsFromJson(Map<String, dynamic> json) {
+  return CitationsArguments(
+    slug: json['slug'] as String,
+  );
+}
+
+Map<String, dynamic> _$CitationsArgumentsToJson(CitationsArguments instance) =>
+    <String, dynamic>{
+      'slug': instance.slug,
+    };
