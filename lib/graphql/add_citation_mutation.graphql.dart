1e72c8541cc4119b3a00e4accf59dbc0006dfb23
Margarita Krutikova <margaretkru@gmail.com>
2020-03-12 00:24:29 +0100

    Add mutation and generated types

---
+++ lib/graphql/add_citation_mutation.graphql.dart
@@ -0,0 +1,261 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:meta/meta.dart';
+import 'package:artemis/artemis.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:equatable/equatable.dart';
+import 'package:gql/ast.dart';
+part 'add_citation_mutation.graphql.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class AddCitation$mutation_root$citations_mutation_response$citations$authors
+    with EquatableMixin {
+  AddCitation$mutation_root$citations_mutation_response$citations$authors();
+
+  factory AddCitation$mutation_root$citations_mutation_response$citations$authors.fromJson(
+          Map<String, dynamic> json) =>
+      _$AddCitation$mutation_root$citations_mutation_response$citations$authorsFromJson(
+          json);
+
+  int id;
+
+  String name;
+
+  @override
+  List<Object> get props => [id, name];
+  Map<String, dynamic> toJson() =>
+      _$AddCitation$mutation_root$citations_mutation_response$citations$authorsToJson(
+          this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class AddCitation$mutation_root$citations_mutation_response$citations
+    with EquatableMixin {
+  AddCitation$mutation_root$citations_mutation_response$citations();
+
+  factory AddCitation$mutation_root$citations_mutation_response$citations.fromJson(
+          Map<String, dynamic> json) =>
+      _$AddCitation$mutation_root$citations_mutation_response$citationsFromJson(
+          json);
+
+  int id;
+
+  String text;
+
+  DateTime added;
+
+  AddCitation$mutation_root$citations_mutation_response$citations$authors
+      author;
+
+  @override
+  List<Object> get props => [id, text, added, author];
+  Map<String, dynamic> toJson() =>
+      _$AddCitation$mutation_root$citations_mutation_response$citationsToJson(
+          this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class AddCitation$mutation_root$citations_mutation_response
+    with EquatableMixin {
+  AddCitation$mutation_root$citations_mutation_response();
+
+  factory AddCitation$mutation_root$citations_mutation_response.fromJson(
+          Map<String, dynamic> json) =>
+      _$AddCitation$mutation_root$citations_mutation_responseFromJson(json);
+
+  List<AddCitation$mutation_root$citations_mutation_response$citations>
+      returning;
+
+  @override
+  List<Object> get props => [returning];
+  Map<String, dynamic> toJson() =>
+      _$AddCitation$mutation_root$citations_mutation_responseToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class AddCitation$mutation_root with EquatableMixin {
+  AddCitation$mutation_root();
+
+  factory AddCitation$mutation_root.fromJson(Map<String, dynamic> json) =>
+      _$AddCitation$mutation_rootFromJson(json);
+
+  AddCitation$mutation_root$citations_mutation_response insert_citations;
+
+  @override
+  List<Object> get props => [insert_citations];
+  Map<String, dynamic> toJson() => _$AddCitation$mutation_rootToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class AddCitationArguments extends JsonSerializable with EquatableMixin {
+  AddCitationArguments(
+      {@required this.text,
+      @required this.collectionId,
+      @required this.authorName,
+      @required this.date});
+
+  factory AddCitationArguments.fromJson(Map<String, dynamic> json) =>
+      _$AddCitationArgumentsFromJson(json);
+
+  final String text;
+
+  final String collectionId;
+
+  final String authorName;
+
+  final DateTime date;
+
+  @override
+  List<Object> get props => [text, collectionId, authorName, date];
+  Map<String, dynamic> toJson() => _$AddCitationArgumentsToJson(this);
+}
+
+class AddCitationMutation
+    extends GraphQLQuery<AddCitation$mutation_root, AddCitationArguments> {
+  AddCitationMutation({this.variables});
+
+  @override
+  final DocumentNode document = DocumentNode(definitions: [
+    OperationDefinitionNode(
+        type: OperationType.mutation,
+        name: NameNode(value: 'AddCitation'),
+        variableDefinitions: [
+          VariableDefinitionNode(
+              variable: VariableNode(name: NameNode(value: 'text')),
+              type: NamedTypeNode(
+                  name: NameNode(value: 'String'), isNonNull: true),
+              defaultValue: DefaultValueNode(value: null),
+              directives: []),
+          VariableDefinitionNode(
+              variable: VariableNode(name: NameNode(value: 'collectionId')),
+              type:
+                  NamedTypeNode(name: NameNode(value: 'uuid'), isNonNull: true),
+              defaultValue: DefaultValueNode(value: null),
+              directives: []),
+          VariableDefinitionNode(
+              variable: VariableNode(name: NameNode(value: 'authorName')),
+              type: NamedTypeNode(
+                  name: NameNode(value: 'String'), isNonNull: true),
+              defaultValue: DefaultValueNode(value: null),
+              directives: []),
+          VariableDefinitionNode(
+              variable: VariableNode(name: NameNode(value: 'date')),
+              type:
+                  NamedTypeNode(name: NameNode(value: 'date'), isNonNull: true),
+              defaultValue: DefaultValueNode(value: null),
+              directives: [])
+        ],
+        directives: [],
+        selectionSet: SelectionSetNode(selections: [
+          FieldNode(
+              name: NameNode(value: 'insert_citations'),
+              alias: null,
+              arguments: [
+                ArgumentNode(
+                    name: NameNode(value: 'objects'),
+                    value: ObjectValueNode(fields: [
+                      ObjectFieldNode(
+                          name: NameNode(value: 'text'),
+                          value: VariableNode(name: NameNode(value: 'text'))),
+                      ObjectFieldNode(
+                          name: NameNode(value: 'collectionId'),
+                          value: VariableNode(
+                              name: NameNode(value: 'collectionId'))),
+                      ObjectFieldNode(
+                          name: NameNode(value: 'added'),
+                          value: VariableNode(name: NameNode(value: 'date'))),
+                      ObjectFieldNode(
+                          name: NameNode(value: 'author'),
+                          value: ObjectValueNode(fields: [
+                            ObjectFieldNode(
+                                name: NameNode(value: 'data'),
+                                value: ObjectValueNode(fields: [
+                                  ObjectFieldNode(
+                                      name: NameNode(value: 'name'),
+                                      value: VariableNode(
+                                          name: NameNode(value: 'authorName'))),
+                                  ObjectFieldNode(
+                                      name: NameNode(value: 'collectionId'),
+                                      value: VariableNode(
+                                          name:
+                                              NameNode(value: 'collectionId')))
+                                ])),
+                            ObjectFieldNode(
+                                name: NameNode(value: 'on_conflict'),
+                                value: ObjectValueNode(fields: [
+                                  ObjectFieldNode(
+                                      name: NameNode(value: 'constraint'),
+                                      value: EnumValueNode(
+                                          name: NameNode(
+                                              value:
+                                                  'authors_name_collectionId_key'))),
+                                  ObjectFieldNode(
+                                      name: NameNode(value: 'update_columns'),
+                                      value: EnumValueNode(
+                                          name: NameNode(value: 'name')))
+                                ]))
+                          ]))
+                    ]))
+              ],
+              directives: [],
+              selectionSet: SelectionSetNode(selections: [
+                FieldNode(
+                    name: NameNode(value: 'returning'),
+                    alias: null,
+                    arguments: [],
+                    directives: [],
+                    selectionSet: SelectionSetNode(selections: [
+                      FieldNode(
+                          name: NameNode(value: 'id'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: null),
+                      FieldNode(
+                          name: NameNode(value: 'text'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: null),
+                      FieldNode(
+                          name: NameNode(value: 'added'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: null),
+                      FieldNode(
+                          name: NameNode(value: 'author'),
+                          alias: null,
+                          arguments: [],
+                          directives: [],
+                          selectionSet: SelectionSetNode(selections: [
+                            FieldNode(
+                                name: NameNode(value: 'id'),
+                                alias: null,
+                                arguments: [],
+                                directives: [],
+                                selectionSet: null),
+                            FieldNode(
+                                name: NameNode(value: 'name'),
+                                alias: null,
+                                arguments: [],
+                                directives: [],
+                                selectionSet: null)
+                          ]))
+                    ]))
+              ]))
+        ]))
+  ]);
+
+  @override
+  final String operationName = 'AddCitation';
+
+  @override
+  final AddCitationArguments variables;
+
+  @override
+  List<Object> get props => [document, operationName, variables];
+  @override
+  AddCitation$mutation_root parse(Map<String, dynamic> json) =>
+      AddCitation$mutation_root.fromJson(json);
+}
---
+++ lib/graphql/add_citation_mutation.graphql.g.dart
@@ -0,0 +1,101 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'add_citation_mutation.graphql.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AddCitation$mutation_root$citations_mutation_response$citations$authors
+    _$AddCitation$mutation_root$citations_mutation_response$citations$authorsFromJson(
+        Map<String, dynamic> json) {
+  return AddCitation$mutation_root$citations_mutation_response$citations$authors()
+    ..id = json['id'] as int
+    ..name = json['name'] as String;
+}
+
+Map<String, dynamic>
+    _$AddCitation$mutation_root$citations_mutation_response$citations$authorsToJson(
+            AddCitation$mutation_root$citations_mutation_response$citations$authors
+                instance) =>
+        <String, dynamic>{
+          'id': instance.id,
+          'name': instance.name,
+        };
+
+AddCitation$mutation_root$citations_mutation_response$citations
+    _$AddCitation$mutation_root$citations_mutation_response$citationsFromJson(
+        Map<String, dynamic> json) {
+  return AddCitation$mutation_root$citations_mutation_response$citations()
+    ..id = json['id'] as int
+    ..text = json['text'] as String
+    ..added =
+        json['added'] == null ? null : DateTime.parse(json['added'] as String)
+    ..author = json['author'] == null
+        ? null
+        : AddCitation$mutation_root$citations_mutation_response$citations$authors
+            .fromJson(json['author'] as Map<String, dynamic>);
+}
+
+Map<String, dynamic>
+    _$AddCitation$mutation_root$citations_mutation_response$citationsToJson(
+            AddCitation$mutation_root$citations_mutation_response$citations
+                instance) =>
+        <String, dynamic>{
+          'id': instance.id,
+          'text': instance.text,
+          'added': instance.added?.toIso8601String(),
+          'author': instance.author?.toJson(),
+        };
+
+AddCitation$mutation_root$citations_mutation_response
+    _$AddCitation$mutation_root$citations_mutation_responseFromJson(
+        Map<String, dynamic> json) {
+  return AddCitation$mutation_root$citations_mutation_response()
+    ..returning = (json['returning'] as List)
+        ?.map((e) => e == null
+            ? null
+            : AddCitation$mutation_root$citations_mutation_response$citations
+                .fromJson(e as Map<String, dynamic>))
+        ?.toList();
+}
+
+Map<String, dynamic>
+    _$AddCitation$mutation_root$citations_mutation_responseToJson(
+            AddCitation$mutation_root$citations_mutation_response instance) =>
+        <String, dynamic>{
+          'returning': instance.returning?.map((e) => e?.toJson())?.toList(),
+        };
+
+AddCitation$mutation_root _$AddCitation$mutation_rootFromJson(
+    Map<String, dynamic> json) {
+  return AddCitation$mutation_root()
+    ..insert_citations = json['insert_citations'] == null
+        ? null
+        : AddCitation$mutation_root$citations_mutation_response.fromJson(
+            json['insert_citations'] as Map<String, dynamic>);
+}
+
+Map<String, dynamic> _$AddCitation$mutation_rootToJson(
+        AddCitation$mutation_root instance) =>
+    <String, dynamic>{
+      'insert_citations': instance.insert_citations?.toJson(),
+    };
+
+AddCitationArguments _$AddCitationArgumentsFromJson(Map<String, dynamic> json) {
+  return AddCitationArguments(
+    text: json['text'] as String,
+    collectionId: json['collectionId'] as String,
+    authorName: json['authorName'] as String,
+    date: json['date'] == null ? null : DateTime.parse(json['date'] as String),
+  );
+}
+
+Map<String, dynamic> _$AddCitationArgumentsToJson(
+        AddCitationArguments instance) =>
+    <String, dynamic>{
+      'text': instance.text,
+      'collectionId': instance.collectionId,
+      'authorName': instance.authorName,
+      'date': instance.date?.toIso8601String(),
+    };
