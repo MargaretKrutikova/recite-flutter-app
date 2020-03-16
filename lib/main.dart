d18519ccba271afcdb0fe6492c01021e78ff4af8
Margarita Krutikova <margaretkru@gmail.com>
2020-03-16 23:37:55 +0100

    Add app theme with colors and text theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:recite_flutter/theme.dart';
 
 import './graphql_client.dart';
 import './citations_page.dart';
 
 void main() => runApp(ReciteApp());
 
-final GRAPHQL_ENDPOINT = 'https://recite-api.margareta.dev/v1/graphql';
-
 class ReciteApp extends StatelessWidget {
   Widget _buildApp(BuildContext context, GraphQLClient client) {
     return MaterialApp(
       title: 'Recite',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        textTheme: TextTheme(
-          headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          body1: TextStyle(fontSize: 22.0, fontFamily: 'Hind'),
-        ),
-      ),
+      theme: appThemeData,
       home: CitationsPage(
           title: 'Citations Page', collectionSlug: "test", client: client),
     );
@@ -28,7 +20,7 @@ class ReciteApp extends StatelessWidget {
   // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
-    final client = clientFor(uri: GRAPHQL_ENDPOINT);
+    final client = createClient();
 
     return ClientProvider(
         client: client, child: _buildApp(context, client.value));
---
+++ lib/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // light green
+  static const primaryColor = const Color(0xFF1bb390);
+  static const darkBackgroundColor = const Color(0xFF353d4a);
+}
+
+final TextTheme appTextTheme = new TextTheme(
+  headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+  title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+  body1: TextStyle(fontSize: 20.0, fontFamily: 'Hind'),
+);
+
+final ThemeData appThemeData = new ThemeData(
+    brightness: Brightness.dark,
+    appBarTheme: AppBarTheme(color: AppColors.primaryColor),
+    backgroundColor: AppColors.darkBackgroundColor,
+    cardColor: AppColors.darkBackgroundColor,
+    textTheme: appTextTheme);
